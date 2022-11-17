953eef9c0776b282c93c2a87641352074aa0ca32
Elango K <107238478+Elangokulandhaivel@users.noreply.github.com>
2022-11-17 22:05:30 +0530

    Add files via upload

---
+++ Project development phase/sprint III/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+import pickle
+
+model = pickle.load(open(r"D:\IBM\model\model.pkl", 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template("reg.html")
+
+
+@app.route('/target.html', methods=['POST'])
+def target():
+    a = request.form['10']
+    b = request.form['12']
+    c = request.form['col']
+    d = request.form['se']
+    e = request.form['c']
+    f = request.form['r']
+    input = [[int(a), int(b), int(c), int(d), int(e), int(f)]]
+    output = model.predict(input)
+    output*=100
+    print(output)
+
+    return render_template("target.html", y= str(output[0][0]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
